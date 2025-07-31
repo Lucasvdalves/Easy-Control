3a6a030961fe547930f8fa15de5358f13626f8de
Damasceno <lucasalvesbh2018@gmail.com>
2025-07-30 21:07:31 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import './App.css'; // Mantenha a importação, mesmo que o arquivo esteja vazio por enquanto
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Easy Control - Nosso Projeto FEFO</h1>
+      <p>Seja bem-vindo!</p>
+    </div>
+  );
+}
+
+export default App;
